73c9ca3f71d13ec0dbc06d8d4029bac74479c51a
Msarker1 <Sarkermt1@gmail.com>
2017-10-03 22:00:40 -0400

    Add files via upload

---
+++ src/res/Assets/scripts/shot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shot : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+
+    private Animator animator;
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb2D = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // rb2D.AddForce(transform.right * 500f);
+    }
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "enemy")
+        {
+            Destroy(gameObject);
+            collision.gameObject.SendMessage("ApplyDamage", 5f);
+        }
+    }
+}
